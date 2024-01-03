24bf91eb5d8aa4a2f1e1924629e069e22cc5fc79
Quan Zheng <q.zheng-7@student.tudelft.nl>
2024-01-03 18:45:24 +0100

    Temporarily fix build errors and add rich text rendering

--- src/lib/contentful/richText.tsx
+++ src/lib/contentful/richText.tsx
@@ -1,49 +1,70 @@
-import { BLOCKS, INLINES } from "@contentful/rich-text-types";
-import type { Document as RichTextDocument } from "@contentful/rich-text-types";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { BLOCKS, INLINES, type Inline } from "@contentful/rich-text-types";
+import type {
+  Document as RichTextDocument,
+  Node,
+} from "@contentful/rich-text-types";
+import {
+  documentToReactComponents,
+  type Options,
+} from "@contentful/rich-text-react-renderer";
 import Image from "next/image";
 
-// Create a bespoke renderOptions object to target BLOCKS.EMBEDDED_ENTRY (linked block entries e.g. code blocks)
-// INLINES.EMBEDDED_ENTRY (linked inline entries e.g. a reference to another blog post)
-// and BLOCKS.EMBEDDED_ASSET (linked assets e.g. images)
-
-const renderOptions = {
-  renderText: (text: string) => {
-    return text.split("\n").reduce((children, textSegment, index) => {
-      return [...children, index > 0 && <br key={index} />, textSegment];
-    }, []);
-  },
-
+const renderOptions: Options = {
   renderNode: {
-    [INLINES.EMBEDDED_ENTRY]: (node, children) => {
+    [BLOCKS.PARAGRAPH]: (node, children) => {
+      return (
+        <p>
+          {children}
+          <br />
+        </p>
+      );
+    },
+    [INLINES.EMBEDDED_ENTRY]: (node: Node, children) => {
       // target the contentType of the EMBEDDED_ENTRY to display as you need
-      if (node.data.target.sys.contentType.sys.id === "blogPost") {
+      if ((node as Inline).data.target.sys.contentType.sys.id === "blogPost") {
         return (
-          <a href={`/blog/${node.data.target.fields.slug}`}>
-            {" "}
-            {node.data.target.fields.title}
+          <a href={`/blog/${(node as Inline).data.target.fields.slug}`}>
+            {(node as Inline).data.target.fields.title}
           </a>
         );
       }
     },
+    [BLOCKS.HEADING_2]: (node, children) => {
+      return <h2 className="text-3xl">{children}</h2>;
+    },
+    [BLOCKS.HEADING_3]: (node, children) => {
+      return <h3 className="text-xl">{children}</h3>;
+    },
+    [BLOCKS.UL_LIST]: (node, children) => {
+      return <ul className="list-disc pl-8">{children}</ul>;
+    },
+    [BLOCKS.OL_LIST]: (node, children) => {
+      return <ol className="list-decimal pl-8">{children}</ol>;
+    },
+    [BLOCKS.LIST_ITEM]: (node, children) => {
+      return <li>{children}</li>;
+    },
     [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
       // target the contentType of the EMBEDDED_ENTRY to display as you need
-      if (node.data.target.sys.contentType.sys.id === "codeBlock") {
+      if ((node as any).data.target.sys.contentType.sys.id === "codeBlock") {
         return (
           <pre>
-            <code>{node.data.target.fields.code}</code>
+            <code>{(node as any).data.target.fields.code}</code>
           </pre>
         );
       }
 
-      if (node.data.target.sys.contentType.sys.id === "videoEmbed") {
+      if ((node as any).data.target.sys.contentType.sys.id === "videoEmbed") {
         return (
           <div className="w-96">
             <iframe
-              src={node.data.target.fields.embedUrl}
+              src={(node as any).data.target.fields.embedUrl}
               height="100%"
               width="100%"
-              title={node.data.target.fields.title}
+              title={(node as any).data.target.fields.title}
               allowFullScreen={true}
             />
           </div>
@@ -55,10 +76,10 @@ const renderOptions = {
       // render the EMBEDDED_ASSET as you need
       return (
         <Image
-          src={`https:${node.data.target.fields.file.url}`}
-          height={node.data.target.fields.file.details.image.height}
-          width={node.data.target.fields.file.details.image.width}
-          alt={node.data.target.fields.description}
+          src={`https:${(node as any).data.target.fields.file.url}`}
+          height={(node as any).data.target.fields.file.details.image.height}
+          width={(node as any).data.target.fields.file.details.image.width}
+          alt={(node as any).data.target.fields.description}
         />
       );
     },
