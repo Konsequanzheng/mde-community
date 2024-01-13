05e40683d246192beb322b13e0972e21bdfc8d31
Quan Zheng <q.zheng-7@student.tudelft.nl>
2024-01-13 17:06:31 +0100

    Add footer

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../_components/navbar";
+import Footer from "../_components/footer";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
       <Navbar />
       <main className="h-full pt-20">{children}</main>
+      <Footer />
     </div>
   );
 };
---
+++ src/app/_components/footer.tsx
@@ -0,0 +1,41 @@
+import { Facebook, Instagram, Mail } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex h-56 flex-row items-center justify-between bg-blue-800 p-20">
+      <Image
+        src="/logo-full-white.png"
+        alt="MDE Community Logo"
+        width={150}
+        height={150}
+      />
+      <div className="flex flex-row gap-x-20 text-white">
+        <div className="flex flex-col gap-y-2">
+          <div className="font-bold">Links</div>
+          <Link href="#communities">Communities</Link>
+          <Link href="#events">Events</Link>
+          <Link href="#blog">Blog</Link>
+        </div>
+        <div className="flex flex-col gap-y-2">
+          <div className="font-bold">Get in touch</div>
+          <div className="flex flex-row gap-x-2">
+            <Link href="https://www.instagram.com/mdecommunity/">
+              <Instagram />
+            </Link>
+            {/* <Link href="">
+            <Facebook />
+            </Link> */}
+            <Link href="mailto:hello@mdecommunity.com">
+              <Mail />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
